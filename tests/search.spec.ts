59219e65ea73b2f8184fd24ea3c1e1ce3294271c
igo0r <igor.lantushenko@gmail.com>
2024-09-25 18:58:21 +0200

    Update search.spec.ts

--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -1,6 +1,15 @@
 import { test, expect } from '@playwright/test';
 
-test('Search for mouse click', async ({ page }) => {
+test('Search for mouse click test #1', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('[aria-label="Search"]').click();
+  await page.locator('#docsearch-input').fill('click');
+  await page.locator('.DocSearch-Hit-title').filter({hasText: 'Mouse'}).click();
+
+  await expect(await page.locator('mouse-click')).toBeVisible();
+});
+
+test('Search for mouse click test #2', async ({ page }) => {
   await page.goto('/');
   await page.locator('[aria-label="Search"]').click();
   await page.locator('#docsearch-input').fill('click');
